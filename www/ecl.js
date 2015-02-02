c36702dad73af01cce7ce37dbafa402501608b7a
evrimulu <evrimulu@gmail.com>
2015-02-02 16:37:49 +0900

    fix javascript module name.

--- www/ecl.js
+++ www/ecl.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.coolMethod = function(arg0, success, error) {
+exports.coolMethod = function(arg0, success, error) {
     exec(success, error, "CordovaEcl", "coolMethod", [arg0]);
 };
